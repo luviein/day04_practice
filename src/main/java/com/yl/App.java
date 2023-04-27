7339e3eec0eb8c93a61fcb206494c1d5366e18f8
Yenleng <yenlengggggg@gmail.com>
2023-04-27 16:41:08 +0800

    add server

--- src/main/java/com/yl/App.java
+++ src/main/java/com/yl/App.java
@@ -1,13 +1,84 @@
 package com.yl;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws NumberFormatException, IOException
     {
-        System.out.println( "Hello World!" );
+        String fullFilePath = args[0];
+        String port = args[1];
+        
+
+        //check if full Path Exists
+        File cookieFile = new File(fullFilePath);
+
+        if(!cookieFile.exists()){
+            System.out.println("Cookie File dont exist.");
+            System.exit(0);
+        }
+        Cookie cookie = new Cookie();
+
+        //establish server
+        ServerSocket server = new ServerSocket(Integer.parseInt(port));
+        Socket socket = server.accept();
+
+        String msgReceived = ""; //store data sent from client
+
+        try(InputStream is = socket.getInputStream()){
+            BufferedInputStream bis = new BufferedInputStream(is);
+            DataInputStream dis = new DataInputStream(bis);
+            
+
+            try(OutputStream os = socket.getOutputStream()){
+                BufferedOutputStream bos = new BufferedOutputStream(os);
+                DataOutputStream dos = new DataOutputStream(bos);
+
+
+                while(!msgReceived.equals("close")){
+                    msgReceived = dis.readUTF();
+                    if(msgReceived.equalsIgnoreCase("get-cookie")){
+                        String randomCookie = cookie.getRandomCookie();
+                        dos.writeUTF(randomCookie);
+                        dos.flush();
+
+                    }else{
+                        dos.writeUTF("");
+                        dos.flush();
+                    }
+
+
+                }
+                dos.close();
+                bos.close();
+                os.close();    
+
+            }catch(EOFException e){
+                e.printStackTrace();
+            }
+
+            dis.close();
+            bis.close();
+            is.close();
+        }catch(EOFException e){
+            socket.close();
+            server.close();
+        }
+
+
     }
 }
--- src/main/java/com/yl/Cookie.java
+++ src/main/java/com/yl/Cookie.java
@@ -2,11 +2,12 @@ package com.yl;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 public class Cookie {
     List<String>cookie = null;
@@ -32,13 +33,37 @@ public class Cookie {
         // close the BufferedReader in reverse order
 
         br.close();
+        }
 
 
+    public String getRandomCookie(){
+
+        // use Math random function to get random item from cookies collection object
+        Random random = new Random();
+
+        //check if cookies collection are loaded, else get random cookie
+        if (cookie.isEmpty()){
+            return "There are no cookies in the jar.";
+        }else{
+            String randomCookie = cookie.get(random.nextInt(cookie.size()));
+            return randomCookie;
+        }
+            
+        
+    
     }
+
+            
+
+}
+            
+
+
+
     
 
 
 
 
 
-}
+
